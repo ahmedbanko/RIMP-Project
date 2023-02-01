832fe110eb68f42072476d626678718439016ef7
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2023-02-01 18:23:43 +0000

    Initial version
    Includes:
    - RIMP lexer, parser, and evaluator
    - Can lex arrays

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+
+object Main extends App {
+  val rimp: RIMP = new RIMP
+//  rimp.testEvaluate()
+//  print(rimp.lexing_simp(rimp.RIMP_REGS, ("arr := [10]")).filter(_._1 != "whitespace"))
+  println(rimp.parse("arr := [10]"))
+}

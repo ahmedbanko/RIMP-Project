4faeb8ab46cba965c0a0f813fb6bbf4f1fda2e53
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2023-02-07 20:09:10 +0000

    Change eval_thread (not complete)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,21 +5,29 @@ object Main extends App {
   val i = new Interpreter()
   val threadProg =
     """a := 1000;
+       xt2 := 0;
       thread t1 := {
-        while (!a > 0) do {
+        write "t1 start\n";
+        while (!a > 1) do {
           skip;
           a := !a - 1
-        }
+        };
+         write "t1 end\n"
       };
       thread t2 := {
-        skip
+        xt2 := 1996;
+        write "t2 start\n";
+        skip;
+        write "t2 end\n"
        };
-      run ?t1;
-      run ?t2""".stripMargin
+        run ?t1;
+        run ?t2;
+        write !a;
+        write !xt2""".stripMargin
   val expected = List(i.AssignThread("t", List(i.If(i.Bop("<",i.Var("a"),i.Var("b")),List(i.Skip),List(i.Assign("a",i.Aop("+",i.Aop("*",i.Var("a"),i.Var("b")),i.Num(1))))))))
 //  println(i.parse(threadProg))
 //  println(i.parse(threadProg) == expected)
-  println(i.eval(i.parse(threadProg)))
+i.eval(i.parse(threadProg))
 }
 
 
--- src/main/scala/rimp/Interpreter.scala
+++ src/main/scala/rimp/Interpreter.scala
@@ -45,16 +45,20 @@ class Interpreter extends Parser {
     values.map(x => eval_aexp(x, env))
 
 
-  def eval_thread(bl: Block, env: Env) : Env = {
-    import java.util.concurrent._
-    val executor = Executors.newSingleThreadExecutor()
-    val callable: Callable[Env] = () => {
-      eval_bl(bl, env)
+
+// TODO: Make sure the thread works correctly
+ var e: Env = Map()
+  def eval_thread(bl: Block, env: Env): Env = {
+    e = e ++ env
+//    this.synchronized {
+    val thread = new Thread {
+      override def run {
+        e = e ++ eval_bl(bl, env)
+      }
     }
-    val future = executor.submit(callable)
-    val result = future.get()
-    executor.shutdown()
-    result
+      thread.start()
+//    }
+    e
   }
 
   def eval_stmt(s: Stmt, env: Env): Env =
@@ -67,6 +71,14 @@ class Interpreter extends Parser {
         val index_eval = eval_aexp(index, env)
         env + (id -> strList2IntList(env(id).toString).updated(index_eval, newVal_eval))
       }
+      case WriteVar(x) =>
+        println(env(x));
+        env
+      case WriteStr(x) =>
+        //Expands standard Scala escape sequences in a string. copied from:
+        //https://www.scala-lang.org/api/2.13.6/scala/StringContext$.html
+        print(StringContext.processEscapes(x.substring(1, x.length - 1)))
+        env
       case AssignThread(id, bl) => env + (id -> bl)
       case RunThread(id) => eval_thread(env(id).asInstanceOf[Block], env)
       case If(b, bl1, bl2) => if (eval_bexp(b, env)) eval_bl(bl1, env) else eval_bl(bl2, env)
@@ -80,6 +92,6 @@ class Interpreter extends Parser {
     case s :: bl => eval_bl(bl, eval_stmt(s, env))
   }
 
-  def eval(bl: Block): Env = eval_bl(bl, Map())
+  def eval(bl: Block): Unit = eval_bl(bl, Map())
 
 }

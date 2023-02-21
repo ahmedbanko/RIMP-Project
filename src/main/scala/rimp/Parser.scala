5a7bd8e8fb8a4966c0393dbb0cb7d3f21c3bae77
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2023-02-21 01:23:02 +0000

    Add space to translate functions

--- src/main/scala/rimp/Parser.scala
+++ src/main/scala/rimp/Parser.scala
@@ -263,7 +263,7 @@ class Parser extends Tokenizer {
     }
     case While(b, bl, counter) => {
       val whileId = counter.id.split("_")(1)
-      s"${counter.id} := ${counter.count};\nwhile-$whileId${stmt2String(b)} do {\n${bl.map(x => stmt2String(x)).mkString(";\n")};\n${counter.id} := !${counter.id} + 1\n}"
+      s"${counter.id} := ${counter.count};\nwhile-$whileId ${stmt2String(b)} do {\n${bl.map(x => stmt2String(x)).mkString(";\n")};\n${counter.id} := !${counter.id} + 1\n}"
     }
     case Assign(s, a) => s"$s := ${stmt2String(a)}"
     case AssignArr(id, values) => s"$id := ${values.mkString("[", ", ", "]")}"
@@ -300,7 +300,7 @@ class Parser extends Tokenizer {
     }
     case While(b, bl, counter) => {
       val whileId = counter.id.split("_")(1)
-      s"while-$whileId(!${counter.id} > 0) do {\n${counter.id} =: !${counter.id} + 1;\n${bl.reverse.map(x => stmt2RevStr(x)).mkString(";\n")}\n};\n${counter.id} =: 0"
+      s"while-$whileId (!${counter.id} > 0) do {\n${counter.id} =: !${counter.id} + 1;\n${bl.reverse.map(x => stmt2RevStr(x)).mkString(";\n")}\n};\n${counter.id} =: 0"
     }
     case Assign(s, a) => s"$s =: ${stmt2RevStr(a)}"
     case AssignArr(id, values) => s"$id =: ${values.mkString("[", ", ", "]")}"
@@ -347,7 +347,7 @@ class Parser extends Tokenizer {
     case hd::tail => revAST(tail, output):+revStmt(hd)
   }
 
-  def rev(ast: List[Stmt]) : String = {
+  def revAst2Code(ast: List[Stmt]) : String = {
     stmts2RevStr(ast.reverse).mkString.split("\n").filterNot(_.isEmpty).mkString("\n")
   }
 

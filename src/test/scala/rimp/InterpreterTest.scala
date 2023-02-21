e469e6e95f1f2d019ba827e8a8d0d35c092e1498
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2023-02-21 13:36:26 +0000

    Fix tests

--- src/test/scala/rimp/InterpreterTest.scala
+++ src/test/scala/rimp/InterpreterTest.scala
@@ -3,6 +3,8 @@ package rimp
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatest.funsuite.AnyFunSuite
 
+import scala.collection.mutable
+
 class InterpreterTest extends AnyFunSuite with BeforeAndAfterAll with BeforeAndAfterEach {
 
   val interp = new Interpreter()
@@ -33,10 +35,10 @@ class InterpreterTest extends AnyFunSuite with BeforeAndAfterAll with BeforeAndA
   test("Test evaluation of arithmatic expressions") {
     assert(interp.eval_aexp(interp.Num(1), env) == 1)
     assertThrows[java.util.NoSuchElementException](interp.eval_aexp(interp.Var("x"), env))
-    env = env + ("x" -> 10)
+    env = env + ("x" -> stack(10))
     assert(interp.eval_aexp(interp.Var("x"), env) == 10)
     assertThrows[java.util.NoSuchElementException](interp.eval_aexp(interp.ArrayVar("arr", interp.Num(0)), env) == 1)
-    env = env + ("arr" -> Array(1,2,3))
+    env = env + ("arr" -> Array(stack(0,1),stack(0,2),stack(0,3)))
     assert(interp.eval_aexp(interp.ArrayVar("arr", interp.Num(0)), env) == 1)
     assert(interp.eval_aexp(interp.Aop("+", interp.Num(1), interp.Num(10)), env) == 11)
     assert(interp.eval_aexp(interp.Aop("-", interp.Num(10), interp.Num(5)), env) == 5)
@@ -71,43 +73,48 @@ class InterpreterTest extends AnyFunSuite with BeforeAndAfterAll with BeforeAndA
     assert(interp.eval_stmt(interp.Skip, env) == env)
     assertThrows[java.util.NoSuchElementException](env("x") == 10)
     env = interp.eval_stmt(interp.Assign("x", interp.Num(10)), env)
-    assert(env("x") == 10)
-    assertThrows[java.util.NoSuchElementException](env("arr") == Array(1,2,3))
+    assert(env("x").asInstanceOf[mutable.Stack[Int]].top == 10)
+    assertThrows[java.util.NoSuchElementException](env("arr") == Array(stack(0,1),stack(0,2),stack(0,3)))
     env = interp.eval_stmt(interp.AssignArr("arr", Array(interp.Num(10), interp.Num(9), interp.Num(8))), env)
-    assert(env("arr").asInstanceOf[Array[Int]] sameElements Array(10, 9, 8))
-    val arr = env("arr").asInstanceOf[Array[Int]]
-    assert(arr.head == 10)
+    assert(env("arr").asInstanceOf[Array[mutable.Stack[Int]]] sameElements Array(stack(0,10),stack(0,9),stack(0,8)))
+    val arr = env("arr").asInstanceOf[Array[mutable.Stack[Int]]]
+    assert(arr.head.top == 10)
     env = interp.eval_stmt(interp.UpdateArrIndex("arr", interp.Num(0),interp.Num(99)), env)
-    val arr2 = env("arr").asInstanceOf[Array[Int]]
-    assert(arr2.head != 10 && arr2.head == 99)
+    val arr2 = env("arr").asInstanceOf[Array[mutable.Stack[Int]]]
+    assert(arr2.head.top != 10 && arr2.head.top == 99)
     env = Map() // clear environment
-    assert(interp.eval_stmt(interp.If(interp.True, List(interp.Skip), List(interp.Assign("i", interp.Num(10)))), env) == env)
-    env = interp.eval_stmt(interp.If(interp.False, List(interp.Skip), List(interp.Assign("i", interp.Num(1)))), env)
-    assert(env("i").asInstanceOf[Int] == 1)
+    assert(interp.eval_stmt(interp.If(interp.True, List(interp.Skip), List(interp.Assign("i", interp.Num(10))), interp.IfResult("id", mutable.Stack[Int](0))), env) == env + ("id" -> stack( 0, 1)))
+    env = interp.eval_stmt(interp.If(interp.False, List(interp.Skip), List(interp.Assign("i", interp.Num(1))), interp.IfResult("id", mutable.Stack[Int](0))), env)
+    assert(env("id").asInstanceOf[mutable.Stack[Int]] == stack(0, 0))
+    assert(env("i").asInstanceOf[mutable.Stack[Int]].top == 1)
     assert(interp.eval_stmt(interp.While(interp.False, List(interp.Skip), interp.Counter(counterID(), 0)), env) == env)
     env = interp.eval_stmt(interp.While(interp.Bop(">", interp.Var("i"), interp.Num(0)), List(interp.Assign("i", interp.Num(0))), interp.Counter(counterID(), 0)), env)
-    assert(env("i").asInstanceOf[Int] == 0)
+    assert(env("i").asInstanceOf[mutable.Stack[Int]].top == 0)
     env = Map() // clear environment
     env = interp.eval_stmt(interp.ArrayWithSize("arr",interp.Num(10)), env)
-    assert(env("arr").asInstanceOf[Array[Int]].length == 10)
-    assert(env("arr").asInstanceOf[Array[Int]](0) == 0)
-    assert(env("arr").asInstanceOf[Array[Int]](9) == 0)
-    assertThrows[java.lang.ArrayIndexOutOfBoundsException](env("arr").asInstanceOf[Array[Int]](-1) == 0)
-    assertThrows[java.lang.ArrayIndexOutOfBoundsException](env("arr").asInstanceOf[Array[Int]](10) == 0)
+    assert(env("arr").asInstanceOf[Array[mutable.Stack[Int]]].length == 10)
+    assert(env("arr").asInstanceOf[Array[mutable.Stack[Int]]](0).top == 0)
+    assert(env("arr").asInstanceOf[Array[mutable.Stack[Int]]](9).top == 0)
+    assertThrows[java.lang.ArrayIndexOutOfBoundsException](env("arr").asInstanceOf[Array[mutable.Stack[Int]]](-1).top == 0)
+    assertThrows[java.lang.ArrayIndexOutOfBoundsException](env("arr").asInstanceOf[Array[mutable.Stack[Int]]](10).top == 0)
   }
 
   test("Test evaluation of blocks") {
     assert(interp.eval_bl(List(), env) == env)
     env = interp.eval_bl(List(interp.Assign("i10", interp.Num(10)), interp.Assign("i11", interp.Num(11))), env)
-    assert(env("i10").asInstanceOf[Int] == 10)
-    assert(env("i11").asInstanceOf[Int] == 11)
+    assert(env("i10").asInstanceOf[mutable.Stack[Int]] == stack(0, 10))
+    assert(env("i10").asInstanceOf[mutable.Stack[Int]].top == 10)
+    assert(env("i11").asInstanceOf[mutable.Stack[Int]] == stack(0, 11))
+    assert(env("i11").asInstanceOf[mutable.Stack[Int]].top == 11)
   }
 
   test("Test eval function") {
     assert(interp.eval(List()) == env)
     env = interp.eval(List(interp.Assign("i10", interp.Num(10)), interp.Assign("i11", interp.Num(11))))
-    assert(env("i10").asInstanceOf[Int] == 10)
-    assert(env("i11").asInstanceOf[Int] == 11)
+    assert(env("i10").asInstanceOf[mutable.Stack[Int]] == stack(0, 10))
+    assert(env("i10").asInstanceOf[mutable.Stack[Int]].top == 10)
+    assert(env("i11").asInstanceOf[mutable.Stack[Int]] == stack(0, 11))
+    assert(env("i11").asInstanceOf[mutable.Stack[Int]].top == 11)
   }
 
 
@@ -126,38 +133,50 @@ class InterpreterTest extends AnyFunSuite with BeforeAndAfterAll with BeforeAndA
 
   test("Test creating empty array with size should have a correct size") {
     env = interp.eval(interp.parse("arr := |10|"))
-    assert(env("arr").asInstanceOf[Array[Int]].length == 10)
+    assert(env("arr").asInstanceOf[Array[mutable.Stack[Int]]].length == 10)
   }
 
   test("Assigning array indexes should work correctly") {
     env = interp.eval(interp.parse(arrProg))
-    assert(env("i").asInstanceOf[Int] == 10)
-    assert(env("ii").asInstanceOf[Int] == 10)
-    assert(env("x").asInstanceOf[Int] == 9)
-    val arr = env("arr").asInstanceOf[Array[Int]]
+    assert(env("i").asInstanceOf[mutable.Stack[Int]].top == 10)
+    assert(env("ii").asInstanceOf[mutable.Stack[Int]].top == 10)
+    assert(env("x").asInstanceOf[mutable.Stack[Int]].top == 9)
+    val arr = env("arr").asInstanceOf[Array[mutable.Stack[Int]]]
     for(i <- 0  until 10){
-      assert(arr(i) == i)
+      assert(arr(i).top == i)
     }
   }
 
   test("Test creating an array with values") {
     env = interp.eval(interp.parse("arr := [1, 2, 3]"))
-    assert(env("arr").asInstanceOf[Array[Int]].length == 3)
-    val arr = env("arr").asInstanceOf[Array[Int]]
-    assert(arr(0) == 1)
-    assert(arr(1) == 2)
-    assert(arr(2) == 3)
+    assert(env("arr").asInstanceOf[Array[mutable.Stack[Int]]].length == 3)
+    val arr = env("arr").asInstanceOf[Array[mutable.Stack[Int]]]
+    assert(arr(0).top == 1)
+    assert(arr(0)== stack(0, 1))
+    assert(arr(1).top  == 2)
+    assert(arr(1)== stack(0, 2))
+    assert(arr(2).top == 3)
+    assert(arr(2)== stack(0, 3))
   }
 
   test("Test getting value from array indexes") {
     env = interp.eval(interp.parse("arr := [1, 2, 3]; i0 := arr[0]; i1 := arr[1]; i2 := arr[2]"))
-    assert(env("arr").asInstanceOf[Array[Int]].length == 3)
-    assert(env("i0").asInstanceOf[Int] == 1)
-    assert(env("i1").asInstanceOf[Int] == 2)
-    assert(env("i2").asInstanceOf[Int] == 3)
+    assert(env("arr").asInstanceOf[Array[mutable.Stack[Int]]].length == 3)
+    assert(env("i0").asInstanceOf[mutable.Stack[Int]].top == 1)
+    assert(env("i1").asInstanceOf[mutable.Stack[Int]].top  == 2)
+    assert(env("i2").asInstanceOf[mutable.Stack[Int]].top  == 3)
     assertThrows[java.lang.ArrayIndexOutOfBoundsException](interp.eval(interp.parse("i3 := arr[3]"), env))
     assertThrows[java.lang.ArrayIndexOutOfBoundsException](interp.eval(interp.parse("i := arr[-1]"), env))
   }
 
+
+
+  def stack(numbers: Int*) : mutable.Stack[Int] = {
+    val stack = mutable.Stack[Int]()
+    for (num <- numbers) {
+      stack.push(num)
+    }
+    stack
+  }
 
 }

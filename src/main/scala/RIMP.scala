fae1d1f9ffd883b6cc9ebe110b7f42991b016f43
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2023-03-12 09:09:59 +0000

    fix tests and put RIMP code back

--- src/main/scala/RIMP.scala
+++ src/main/scala/RIMP.scala
@@ -1,252 +1,130 @@
 import rimp.Interpreter
 
+import scala.io.Source
+
 object RIMP extends App {
-//  val i = new Interpreter()
-//  println("Welcome to RIMP Interpreter!")
-//  println("Type help to see possible commands and options.")
-//  var exit = false
-//  while (!exit) {
-//    val input = scala.io.StdIn.readLine()
-//    if (input.isEmpty) println("Error: Please enter a command or type 'help' for available options.")
-//    else {
-//      val parts = input.split("\\s+")
-//      val command = parts.head
-//      val args = parts.tail.toList
-//        command match {
-//          case "translate" => translate(args)
-//          case "invert" => invert(args)
-//          case "evaluate" => evaluate(args)
-//          case "reverse" => reverse(args)
-//          case "help" => help()
-//          case "exit" => exit = true
-//          case _ => println(s"Unknown command: $command")
-//        }
-//    }
-//    i.resetCounters
-//  }
-//
-//
-//  def translate(args: List[String]): Unit = {
-//    if (args.isEmpty)
-//      println("Error: please type a file name e.g. 'EX1'")
-//    else {
-//      val code = readFile(args.head)
-//      if (code.startsWith("_Error")) println(code.tail)
-//      else println(i.translate(code))
-//    }
-//  }
-//
-//  def invert(args: List[String]): Unit = {
-//    if (args.isEmpty)
-//      println("Error: please type a file name e.g. 'EX1'")
-//    else {
-//      val code = readFile(args.head)
-//      if (code.startsWith("_Error")) println(code.tail)
-//      else println(i.translateRev(code))
-//    }
-//  }
-//
-//  def getOption(args: List[String]): String = args match {
-//    case Nil => ""
-//    case "--steps"::_ => "steps"
-//    case _ => "_Error: available options is: '--steps'"
-//  }
-//
-//  def evaluate(args: List[String]): Unit = {
-//    if (args.isEmpty)
-//      println("Error: please type a file name e.g. 'EX1'")
-//    else {
-//      val code = readFile(args.head)
-//      if (code.startsWith("_Error")) println(code.tail)
-//      else {
-//        val option = getOption(args.tail)
-//        val ast = i.parse(code)
-//
-//          if (option.startsWith("_Error")){
-//          println(option.tail)
-//        }else if (option.equals("steps")){
-//          val env = i.eval(ast, printSteps = true)
-//          println(i.stack_tops(env))
-//        }else{
-//          val env = i.eval(ast)
-//          println(i.stack_tops(env))
-//        }
-//      }
-//    }
-//  }
-//
-//  def reverse(args: List[String]): Unit = {
-//    if (args.isEmpty)
-//      println("Error: please type a file name e.g. 'EX1'")
-//    else {
-//      val code = readFile(args.head)
-//      if (code.startsWith("_Error")) println(code.tail)
-//      else {
-//        val option = getOption(args.tail)
-//        val ast = i.parse(code)
-//        val env = i.eval(ast)
-//
-//       if (option.startsWith("_Error")) {
-//          println(option.tail)
-//        } else if (option.equals("steps")) {
-//         val rev_env = i.revEval(ast, env, printSteps = true)
-//         println(i.stack_tops(rev_env))
-//        } else {
-//         val rev_env = i.revEval(ast, env)
-//         println(i.stack_tops(rev_env))
-//        }
-//
-//      }
-//    }
-//  }
-//  def help(): Unit = {
-//    println("Type one of the following commands in the format COMMAND <file_name> --OPTION")
-//    println("'translate' prints the given code with additional variables/counters to it.")
-//    println("'invert' prints reversed representation of the given code.")
-//    println("'evaluate' executes the code in the forward direction and prints the final values of all variables.")
-//    println("'reverse' executes the code in the backward direction and prints the final values of all variables.")
-//    println("NOTE: the commands 'evaluate' & 'reverse' can run with the option '--steps' which prints the runtime values.")
-//  }
-//
-//
-//  def readFile(name: String): String = {
-//    val filename = s"src/main/scala/Examples/$name.rimp"
-//    try {
-//      val source = Source.fromFile(filename)
-//      val output = source.mkString
-//      source.close()
-//      if(output.isEmpty) "_Error reading file: file is empty"
-//      else output
-//    } catch {
-//      case ex: Exception =>
-//        s"_Error reading file: ${ex.getMessage}"
-//    }
-//  }
-
-  val EX1 =
-    """
-     fact := 1;
-     n := 3;
-     while (!n > 0) do {
-          fact := !n * !fact;
-          n := !n - 1
-     }
-    """
-
-  val EX2 =
-    """a := 49;
-            b := 28;
-            while ~(!a = !b) do
-            if !a > !b
-            then a := !a - !b
-            else b := !b - !a"""
-
-  val EX3 =
-    """x := 12;
-while !x > 1 do {
-    r := !x;
-    while !r > 1 do {
-        r := !r - 2
-        };
-    if !r = 0
-    then x := !x / 2
-    else x := 3 * !x + 1}"""
-
-
-  val EX4 =
-    """x := 13;
-factor := 2;
-isprime := 1;
-limit := !x / 2 + 1;
-while !factor < !limit do {
-    r := !x;
-    (while !r > !factor - 1 do
-        r := !r - !factor);
-    (if !r = 0 then isprime := 0 else skip);
-    factor := !factor + 1}"""
-
-  val EX5 =
-    """n := 10;
-a := 1;
-b := 1;
-i := 0;
-while !i < !n do {
-    tmp := !a;
-    a := !b;
-    b := !tmp + !a;
-    i := !i + 1}"""
-
-
-  val arrProg1 =
-    """arr := |10|;
-     i := 0;
-     while (!i < 10) do {
-      arr[!i] := !i;
-      i := !i + 1
-     };
-     ii := 0;
-     while (!ii < 10) do {
-        x := arr[!ii];
-          ii := !ii + 1
-         }"""
-
-  val arrProg2 = "arr := [1+1, 2, 3]; i1before := arr[1]; arr[1] := 10; i1after := arr[1]"
-
-
-  val if_true_prog =
-    """
-    x := 10;
-    y := 100;
-    if (!x > 0) then {
-        x := !x - 1
-    }else{
-        y := !y - 1
-    }
-    """
-
-  val if_false_prog =
-    """
-    x := 10;
-    y := 100;
-    if (!x > 100) then {
-        x := !x - 1
-    }else{
-        y := !y - 1
-    }
-    """
-
-
-  val reverse_arr_prog =
-    """arr := [1, 2, 3, 4, 5];
-    arr_len := 5;
-    left := 0;
-    right := !arr_len - 1;
-    while(!left < !right) do {
-       tmp_left := arr[!left];
-       tmp_right := arr[!right];
-       arr[!right] := !tmp_left;
-        arr[!left] := !tmp_right;
-       left := !left + 1;
-       right := !right - 1
-    }
-   """
-
-
   val i = new Interpreter()
-//  val ast = i.parse(EX1)
-//
-  println(i.translate(EX2))
-  println(i.invert(EX2))
+  println("Welcome to RIMP Interpreter!")
+  println("Type help to see possible commands and options.")
+  var exit = false
+  while (!exit) {
+    val input = scala.io.StdIn.readLine()
+    if (input.isEmpty) println("Error: Please enter a command or type 'help' for available options.")
+    else {
+      val parts = input.split("\\s+")
+      val command = parts.head
+      val args = parts.tail.toList
+        command match {
+          case "translate" => translate(args)
+          case "invert" => invert(args)
+          case "evaluate" => evaluate(args)
+          case "reverse" => reverse(args)
+          case "help" => help()
+          case "exit" => exit = true
+          case _ => println(s"Unknown command: $command")
+        }
+    }
+    i.resetCounters
+  }
 
-//  val env =  i.eval(ast)
-//  println(env)
-//  println(i.stack_tops(env))
-//  val rev_ast = i.revAST(ast)
-//  val rev_env = i.eval(rev_ast, env)
-//  println(rev_env)
-//  println( i.stack_tops(rev_env))
+
+  def translate(args: List[String]): Unit = {
+    if (args.isEmpty)
+      println("Error: please type a file name e.g. 'EX1'")
+    else {
+      val code = readFile(args.head)
+      if (code.startsWith("_Error")) println(code.tail)
+      else println(i.translate(code))
+    }
+  }
+
+  def invert(args: List[String]): Unit = {
+    if (args.isEmpty)
+      println("Error: please type a file name e.g. 'EX1'")
+    else {
+      val code = readFile(args.head)
+      if (code.startsWith("_Error")) println(code.tail)
+      else println(i.invert(code))
+    }
+  }
+
+  def getOption(args: List[String]): String = args match {
+    case Nil => ""
+    case "--steps"::_ => "steps"
+    case _ => "_Error: available options is: '--steps'"
+  }
+
+  def evaluate(args: List[String]): Unit = {
+    if (args.isEmpty)
+      println("Error: please type a file name e.g. 'EX1'")
+    else {
+      val code = readFile(args.head)
+      if (code.startsWith("_Error")) println(code.tail)
+      else {
+        val option = getOption(args.tail)
+        val ast = i.parse(code)
+
+          if (option.startsWith("_Error")){
+          println(option.tail)
+        }else if (option.equals("steps")){
+          val env = i.eval(ast, printSteps = true)
+          println(i.stack_tops(env))
+        }else{
+          val env = i.eval(ast)
+          println(i.stack_tops(env))
+        }
+      }
+    }
+  }
+
+  def reverse(args: List[String]): Unit = {
+    if (args.isEmpty)
+      println("Error: please type a file name e.g. 'EX1'")
+    else {
+      val code = readFile(args.head)
+      if (code.startsWith("_Error")) println(code.tail)
+      else {
+        val option = getOption(args.tail)
+        val ast = i.parse(code)
+        val env = i.eval(ast)
+
+        val rev_ast = i.revAST(ast)
+       if (option.startsWith("_Error")) {
+          println(option.tail)
+        } else if (option.equals("steps")) {
+         val rev_env = i.eval(rev_ast, env, printSteps = true)
+         println(i.stack_tops(rev_env))
+        } else {
+         val rev_env = i.eval(rev_ast, env)
+         println(i.stack_tops(rev_env))
+        }
+
+      }
+    }
+  }
+  def help(): Unit = {
+    println("Type one of the following commands in the format COMMAND <file_name> --OPTION")
+    println("'translate' prints the given code with additional variables/counters to it.")
+    println("'invert' prints reversed representation of the given code.")
+    println("'evaluate' executes the code in the forward direction and prints the final values of all variables.")
+    println("'reverse' executes the code in the backward direction and prints the final values of all variables.")
+    println("NOTE: the commands 'evaluate' & 'reverse' can run with the option '--steps' which prints the runtime values.")
+  }
 
 
+  def readFile(name: String): String = {
+    val filename = s"src/main/scala/Examples/$name.rimp"
+    try {
+      val source = Source.fromFile(filename)
+      val output = source.mkString
+      source.close()
+      if(output.isEmpty) "_Error reading file: file is empty"
+      else output
+    } catch {
+      case ex: Exception =>
+        s"_Error reading file: ${ex.getMessage}"
+    }
+  }
 }
 
 
--- src/test/scala/rimp/ParserTest.scala
+++ src/test/scala/rimp/ParserTest.scala
@@ -14,8 +14,8 @@ class ParserTest extends AnyFunSuite with BeforeAndAfterAll with BeforeAndAfterE
 
   val ex1_translated = "fact := 1;\nn := 3;\n_k1 := 0;\nwhile-1 (!n > 0) do {\nfact := !n * !fact;\nn := !n - 1;\n_k1 := !_k1 + 1\n}"
   val ex2_translated = "a := 49;\nb := 28;\n_k1 := 0;\nwhile-1 ~(!a = !b) do {\n_if1 := 0;\nif-1 (!a > !b) then {\na := !a - !b;\n_if1 := 1\n} else {\nb := !b - !a;\n_if1 := 0\n};\n_k1 := !_k1 + 1\n}"
-  val ex3_translated = "x := 12;\n_k1 := 0;\nwhile-1 (!x > 1) do {\nr := !x;\n_k2 := 0;\nwhile-2 (!r > 1) do {\nr := !r - 2;\n_k2 := !_k2 + 1\n};\n_if1 := 0;\nif-1 (!r = 0) then {\n_if1 := 1;\nx := !x / 2\n} else {\n_if1 := 0;\nx := 3 * !x + 1\n};\n_k1 := !_k1 + 1\n}"
-  val ex4_translated = "x := 13;\nfactor := 2;\nisprime := 1;\nlimit := !x / 2 + 1;\n_k1 := 0;\nwhile-1 (!factor < !limit) do {\nr := !x;\n_k2 := 0;\nwhile-2 (!r > !factor - 1) do {\nr := !r - !factor;\n_k2 := !_k2 + 1\n};\n_if1 := 0;\nif-1 (!r = 0) then {\n_if1 := 1;\nisprime := 0\n} else {\n_if1 := 0;\nskip\n};\nfactor := !factor + 1;\n_k1 := !_k1 + 1\n}"
+  val ex3_translated = "x := 12;\n_k1 := 0;\nwhile-1 (!x > 1) do {\n_if1 := 0;\nr := !x;\n_k2 := 0;\nwhile-2 (!r > 1) do {\nr := !r - 2;\n_k2 := !_k2 + 1\n};\nif-1 (!r = 0) then {\nx := !x / 2;\n_if1 := 1\n} else {\nx := 3 * !x + 1;\n_if1 := 0\n};\n_k1 := !_k1 + 1\n}"
+  val ex4_translated = "x := 13;\nfactor := 2;\nisprime := 1;\nlimit := !x / 2 + 1;\n_k1 := 0;\nwhile-1 (!factor < !limit) do {\n_if1 := 0;\nr := !x;\n_k2 := 0;\nwhile-2 (!r > !factor - 1) do {\nr := !r - !factor;\n_k2 := !_k2 + 1\n};\nif-1 (!r = 0) then {\nisprime := 0;\n_if1 := 1\n} else {\nskip;\n_if1 := 0\n};\nfactor := !factor + 1;\n_k1 := !_k1 + 1\n}"
   val ex5_translated = "n := 10;\na := 1;\nb := 1;\ni := 0;\n_k1 := 0;\nwhile-1 (!i < !n) do {\ntmp := !a;\na := !b;\nb := !tmp + !a;\ni := !i + 1;\n_k1 := !_k1 + 1\n}"
   val arrProg1_translated = "arr := |10|;\ni := 0;\n_k1 := 0;\nwhile-1 (!i < 10) do {\narr[!i] := !i;\ni := !i + 1;\n_k1 := !_k1 + 1\n};\nii := 0;\n_k2 := 0;\nwhile-2 (!ii < 10) do {\nx := arr[!ii];\nii := !ii + 1;\n_k2 := !_k2 + 1\n}"
   val reverse_arr_prog_translated = "arr := [1, 2, 3, 4, 5];\narr_len := 5;\nleft := 0;\nright := !arr_len - 1;\n_k1 := 0;\nwhile-1 (!left < !right) do {\ntmp_left := arr[!left];\ntmp_right := arr[!right];\narr[!right] := !tmp_left;\narr[!left] := !tmp_right;\nleft := !left + 1;\nright := !right - 1;\n_k1 := !_k1 + 1\n}"
@@ -33,14 +33,14 @@ class ParserTest extends AnyFunSuite with BeforeAndAfterAll with BeforeAndAfterE
   }
 
   val ex1_inverted = "while-1 (!_k1 > 0) do {\n_k1 =: !_k1 + 1;\nn =: !n - 1;\nfact =: !n * !fact\n};\n_k1 =: 0;\nn =: 3;\nfact =: 1"
-  val ex2_inverted = "while-1 (!_k1 > 0) do {\n_k1 =: !_k1 + 1;\nif-1 (!_if1 = 1) then {\n_if1 =: 1;\na =: !a - !b\n} else {\n_if1 =: 0;\nb =: !b - !a\n}\n};\n_k1 =: 0;\nb =: 28;\na =: 49"
-  val ex3_inverted = "while-1 (!_k1 > 0) do {\n_k1 =: !_k1 + 1;\nif-1 (!_if1 = 1) then {\n_if1 =: 1;\nx =: !x / 2\n} else {\n_if1 =: 0;\nx =: 3 * !x + 1\n};\nwhile-2 (!_k2 > 0) do {\n_k2 =: !_k2 + 1;\nr =: !r - 2\n};\n_k2 =: 0;\nr =: !x\n};\n_k1 =: 0;\nx =: 12"
-  val ex4_inverted = "while-1 (!_k1 > 0) do {\n_k1 =: !_k1 + 1;\nfactor =: !factor + 1;\nif-1 (!_if1 = 1) then {\n_if1 =: 1;\nisprime =: 0\n} else {\n_if1 =: 0;\nskip\n};\nwhile-2 (!_k2 > 0) do {\n_k2 =: !_k2 + 1;\nr =: !r - !factor\n};\n_k2 =: 0;\nr =: !x\n};\n_k1 =: 0;\nlimit =: !x / 2 + 1;\nisprime =: 1;\nfactor =: 2;\nx =: 13"
+  val ex2_inverted = "while-1 (!_k1 > 0) do {\n_k1 =: !_k1 + 1;\nif-1 (!_if1 = 1) then {\n_if1 =: 1;\na =: !a - !b\n} else {\n_if1 =: 0;\nb =: !b - !a\n};\n_if1 =: 0\n};\n_k1 =: 0;\nb =: 28;\na =: 49"
+  val ex3_inverted = "while-1 (!_k1 > 0) do {\n_k1 =: !_k1 + 1;\nif-1 (!_if1 = 1) then {\n_if1 =: 1;\nx =: !x / 2\n} else {\n_if1 =: 0;\nx =: 3 * !x + 1\n};\nwhile-2 (!_k2 > 0) do {\n_k2 =: !_k2 + 1;\nr =: !r - 2\n};\n_k2 =: 0;\nr =: !x;\n_if1 =: 0\n};\n_k1 =: 0;\nx =: 12"
+  val ex4_inverted = "while-1 (!_k1 > 0) do {\n_k1 =: !_k1 + 1;\nfactor =: !factor + 1;\nif-1 (!_if1 = 1) then {\n_if1 =: 1;\nisprime =: 0\n} else {\n_if1 =: 0;\nskip\n};\nwhile-2 (!_k2 > 0) do {\n_k2 =: !_k2 + 1;\nr =: !r - !factor\n};\n_k2 =: 0;\nr =: !x;\n_if1 =: 0\n};\n_k1 =: 0;\nlimit =: !x / 2 + 1;\nisprime =: 1;\nfactor =: 2;\nx =: 13"
   val ex5_inverted = "while-1 (!_k1 > 0) do {\n_k1 =: !_k1 + 1;\ni =: !i + 1;\nb =: !tmp + !a;\na =: !b;\ntmp =: !a\n};\n_k1 =: 0;\ni =: 0;\nb =: 1;\na =: 1;\nn =: 10"
   val arrProg1_inverted = "while-2 (!_k2 > 0) do {\n_k2 =: !_k2 + 1;\nii =: !ii + 1;\nx =: arr[!ii]\n};\n_k2 =: 0;\nii =: 0;\nwhile-1 (!_k1 > 0) do {\n_k1 =: !_k1 + 1;\ni =: !i + 1;\narr[!i] =: !i\n};\n_k1 =: 0;\ni =: 0;\narr =: |10|"
   val reverse_arr_prog_inverted= "while-1 (!_k1 > 0) do {\n_k1 =: !_k1 + 1;\nright =: !right - 1;\nleft =: !left + 1;\narr[!left] =: !tmp_right;\narr[!right] =: !tmp_left;\ntmp_right =: arr[!right];\ntmp_left =: arr[!left]\n};\n_k1 =: 0;\nright =: !arr_len - 1;\nleft =: 0;\narr_len =: 5;\narr =: [1, 2, 3, 4, 5]"
 
-  test("Test translateRev function") {
+  test("Test invert function") {
     assert(p.invert(fixtures.EX1) == ex1_inverted)
     assert(p.invert(fixtures.EX2) == ex2_inverted)
     assert(p.invert(fixtures.EX3) == ex3_inverted)

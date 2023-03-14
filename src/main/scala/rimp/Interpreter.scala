f71ac2b6f3124662ab66385590872c2911475774
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2023-03-14 15:27:45 +0000

    Add tests for env2string function

--- src/main/scala/rimp/Interpreter.scala
+++ src/main/scala/rimp/Interpreter.scala
@@ -38,41 +38,41 @@ class Interpreter extends Parser {
   def eval_stmt(s: Stmt, env: Env, printSteps: Boolean = false): Env =
     s match {
       case Skip =>
-        if(printSteps) printEnv(env)
+        if(printSteps) println(env2string(env))
         env
       case Assign(x, a) =>
         val old_stack = env.getOrElse(x, stack())
         val out = env + (x -> old_stack.asInstanceOf[RVar].push(eval_aexp(a, env)))
-        if(printSteps) printEnv(out)
+        if(printSteps) println(env2string(out))
         out
       case AssignArr(id, values) =>
         val old_stack = env.getOrElse(id, mutable.Stack(Array.fill(values.length)(stack()))).asInstanceOf[RArray]
         old_stack.push(values.map(v => stack(eval_aexp(v, env))))
         val out = env + (id -> old_stack)
-        if(printSteps) printEnv(out)
+        if(printSteps) println(env2string(out))
         out
       case AssignNewArrWithSize(id, size) =>
         val length = eval_aexp(size, env)
         val old_stack = env.getOrElse(id, mutable.Stack(Array.fill(length)(stack()))).asInstanceOf[RArray]
         old_stack.push(Array.fill(length)(stack()))
         val out = env + (id -> old_stack)
-        if(printSteps) printEnv(out)
+        if(printSteps) println(env2string(out))
         out
       case UpdateArrIndex(id, index, newVal) =>
         val newVal_eval = eval_aexp(newVal, env)
         val index_eval = eval_aexp(index, env)
         val arr = env(id).asInstanceOf[RArray]
         arr.top(index_eval).push(newVal_eval)
-        if(printSteps) printEnv(env)
+        if(printSteps) println(env2string(env))
         env
       case If(b, bl1, bl2, if_id) =>
         if (eval_bexp(b, env)) {
          val out = eval_bl(bl1, env, printSteps)
-          if(printSteps) printEnv(out)
+          if(printSteps) println(env2string(out))
           out
         } else {
           val out = eval_bl(bl2, env, printSteps)
-          if(printSteps) printEnv(out)
+          if(printSteps) println(env2string(out))
           out
         }
 
@@ -81,7 +81,7 @@ class Interpreter extends Parser {
             eval_stmt(While(b, bl, counter), eval_bl(bl, env, printSteps), printSteps)
         }
         else {
-          if(printSteps) printEnv(env)
+          if(printSteps) println(env2string(env))
           env
         }
 
@@ -89,25 +89,25 @@ class Interpreter extends Parser {
         val old_stack = env(x).asInstanceOf[RVar]
         old_stack.pop
         val out = env + (x -> old_stack)
-        if (printSteps) printEnv(out)
+        if (printSteps) println(env2string(out))
         out
       case RevAssignArr(id, _) =>
         val old_stack = env(id).asInstanceOf[RArray]
         old_stack.pop()
         val out = env + (id -> old_stack)
-        if (printSteps) printEnv(out)
+        if (printSteps) println(env2string(out))
         out
       case RevAssignNewArrWithSize(id, _) =>
         val old_stack = env(id).asInstanceOf[RArray]
         old_stack.pop()
         val out = env + (id -> old_stack)
-        if (printSteps) printEnv(out)
+        if (printSteps) println(env2string(out))
         out
       case RevUpdateArrIndex(id, index, _) =>
         val index_eval = eval_aexp(index, env)
         val arr = env(id).asInstanceOf[RArray].top
         arr(index_eval).pop
-        if (printSteps) printEnv(env)
+        if (printSteps) println(env2string(env))
         env
     }
 
@@ -118,7 +118,7 @@ class Interpreter extends Parser {
   }
 
   def eval(bl: Block, env: Env = Map(), printSteps: Boolean = false): Env = {
-    if(printSteps) printEnv(env)
+    if(printSteps) println(env2string(env))
     eval_bl(bl, env, printSteps)
   }
 
@@ -136,14 +136,14 @@ class Interpreter extends Parser {
   }
 
 
-  def printEnv(env: Env): Unit = {
-    println(env.map { case (key, value) =>
+  def env2string(env: Env): String = {
+    env.map { case (key, value) =>
       value match {
         case s: mutable.Stack[_] if s.head.isInstanceOf[Array[RVar]] =>
           s"$key -> ${s.map(a => s"Array${a.asInstanceOf[Array[RVar]].map(a => s"${a.toString}").mkString("(", ", ", ")")}")}"
         case rest => s"$key -> ${rest.toString}"
       }
-    }.mkString("Map(", ", ", ")"))
+    }.mkString("Map(", ", ", ")")
   }
 
 }
